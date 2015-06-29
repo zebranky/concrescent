9b59536f689b59ecd7cf9b79caf9964414e62d79
Andrew Brockert <andrew@mercuric.net>
2015-06-29 11:36:05 -0400

    Slack webhook for staff applications

--- cm/apply-staff/application.php
+++ cm/apply-staff/application.php
@@ -18,3 +18,18 @@ function render_application_body($title) {
 function render_application_tail() {
 	render_tail();
 }
+
+// Trigger a Slack webhook to notify about new applications.
+function staff_application_hook($staffer_id, $first_name, $last_name, $fandom_name) {
+	$url = '';
+	$payload = '{"text": "<https://example.com/admin/review_staffer.php?id='.$staffer_id.'|New staff application> from '.$first_name.' '.$last_name.' ('.$fandom_name.')"}';
+	$params = array('payload' => $payload);
+	$query = http_build_query($params);
+	$contextData = array(
+		'method' => 'POST',
+		'header' => "Connection: close\r\n".
+					"Content-Length: ".strlen($query)."\r\n",
+		'content' => $query);
+	$context = stream_context_create(array('http' => $contextData));
+	$result = file_get_contents($url, false, $context);
+}
